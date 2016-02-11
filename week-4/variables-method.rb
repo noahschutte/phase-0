db5a609031dfdd11b1bbd9b28c86bd8e83301658
Noah Schutte <noahschutte@gmail.com>
2016-02-11 14:58:35 -0500

    add reflection

--- week-4/variables-method.rb
+++ week-4/variables-method.rb
@@ -1,27 +1,25 @@
 **How do you define a local variable?**
 
-
+A local variable does not have an inherent value until it is called and assigned. Local variables should start with a lowercase letter, include letters, numbers, and either hyphens or underscores. A local variable can only be called within it's scope. In some cases that may be within the file, or even within a single method. 
 	
 **How do you define a method?**
 
-
+If a string, number, float, or variable is a noun in the English language, then a method is a verb. It is an action that an object takes or an action that is taken on the object. Methods "do things", whereas nouns simply refer to a value or string. 
 
 **What is the difference between a local variable and a method?**
 
-
+Akin to my example above, a local variable is a value or string that acts like a noun. Methods are actions that either happen to nouns or that nouns enact. 
 
 **How do you run a ruby program from the command line?**
 
-
-
-**How do you run an RSpec file from the command line?**
-
-
+In my VM, I am running linux. On linux, I navigate to the local repository that holds the files that I want to run. For spec files, I run rspec and the file name. For ruby files, I run ruby and the ruby file name. I am also able to run irb for the interactive ruby shell scripting. For this I must "exit" out when I'm done shell scripting. 
 
 **What was confusing about this material? What made sense?**
 
+The most confusing aspect was understanding the syntax of ruby. The tasks were straightforward, but the detail required for successfully running a test set me back the most time.
 
-
+#4.3.1 Challenge
 https://github.com/noahschutte/phase-0/blob/master/week-4/address/my_solution.rb
 
+#4.3.2 Challenge
 https://github.com/noahschutte/phase-0/blob/master/week-4/math/my_solution.rb
