572a96bcf16bbe6c301379bfe8ae1cd39a1d711b
Noah Schutte <noahschutte@gmail.com>
2016-02-24 13:06:27 -0500

    add challenge

--- week-6/test.rb
+++ week-6/test.rb
@@ -1,7 +1,9 @@
-difficulty = {
-	:HTML => "easy",
-	:CSS => "intermediate",
-	:Ruby => "advanced"
-}
+def acct_groups(names)
+  if names.length > 5
+    group = []
+    group = names.each_slice(5).to_a
+    p group
+  end
+end
 
-p difficulty[:Ruby]
+acct_groups(["John","Lisa","Annie","James", "Peter", "Daniel", "Tom", "Paul", "Lara", "Emily", "Andrew", "Tim", "Ray", "Pat", "Jenna", "Kim", "Ben", "Rita", "Wes", "Archie", "Don", "Calvin", "Jason"])
